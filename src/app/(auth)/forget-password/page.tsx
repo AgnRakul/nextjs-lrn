98e42e761357a56b332f677fac7aa7bb88ebda77
RakulAgn <rakul@cartrabbit.in>
2025-01-05 17:13:30 +0530

    feat: learned about route grouping

---
+++ src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,3 @@
+export default function ForgetPassword() {
+  return <div className="flex h-screen w-screen items-center justify-center text-2xl">ForgetPassword</div>;
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function Signup() {
+  return <div className="flex h-screen w-screen items-center justify-center text-2xl">Signup</div>;
+}
