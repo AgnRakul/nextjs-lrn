03bae54c9af2dd6cc55f5c561607e9767304f6d7
RakulAgn <rakul@cartrabbit.in>
2025-01-05 17:13:10 +0530

    feat: learned about private folders

---
+++ src/app/_private/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateFolder() {
+  return <div>This Page Does Not Comes in Routing because its private the folder which contains _ at the first consider as private folder</div>;
+}
