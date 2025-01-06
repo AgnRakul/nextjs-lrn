943120a629e7d73b9f825cff8d8a2b17a585fecd
RakulAgn <rakul@cartrabbit.in>
2025-01-06 14:08:20 +0530

    feat: learned about multiple root layout

---
+++ src/app/(marketing)/revenue/page.tsx
@@ -0,0 +1,3 @@
+export default function Revenue() {
+  return <div className="flex h-screen w-screen items-center justify-center text-2xl">Revenue</div>;
+}
--- src/app/not-found.tsx
+++
@@ -1,3 +0,0 @@
-export default function NotFound() {
-  return <div className="flex h-screen w-screen items-center justify-center text-2xl">Not Found Page</div>;
-}
